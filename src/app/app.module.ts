54ed417543921b3c349658d21b6be1909b44fafe
Arbaaz Khan <arbaazkhanark23@gmail.com>
2023-07-02 16:53:02 +0530

    Routing in Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { TodoComponent } from './MyComponents/todo/todo.component';
 import { TodoItemComponent } from './MyComponents/todo-item/todo-item.component';
 import { InputTodoAddComponent } from './MyComponents/input-todo-add/input-todo-add.component';
+import { AboutComponent } from './MyComponents/about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TodoComponent,
     TodoItemComponent,
-    InputTodoAddComponent
+    InputTodoAddComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
